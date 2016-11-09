32bd9e0278706b0d7a7aeee5bd76b673d80774fd
bcdady <bryan+git@dady.us>
2016-11-08 23:29:23 -0700

    November enhancements to profile
    * Enhance Logic prior to invoking Update-Help
    * New function open-code to launch or open file in VS Code (Insiders);
    also PS (profile) alias 'psedit' opens specified file in VS Code
    (Insiders)

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -74,12 +74,14 @@ Get-Command -Module Microsoft*, Cim*, PS*, ISE |
     Get-Help -ShowWindow
 }
 
+Write-Output -InputObject ''
 # Invoke-Expression (New-Object Net.WebClient).DownloadString('http://bit.ly/e0Mw9w')
 
 <# check and conditionally update/fix PSModulePath
 on Mac, default PSMODULEPATH (yes, it's case sensitive) is: $env:USERPROFILE/.local/share/powershell/Modules;;/usr/local/microsoft/powershell/Modules
 #>
 
+Write-Output -InputObject 'Check Modules Paths: $myPSmodPath'
 if ($IsWindows)
 {
     # Use local $HOME if GPO/UNC $HOME is not available
@@ -109,38 +111,76 @@ if (-not ($myPSmodPath -in @($env:PSMODULEPATH -split $splitChar)))
 {
     # Improve to only conditionally modify 
     # $env:PSMODULEPATH = @("$HOME\Documents\WindowsPowerShell\Modules"; "$pshome\Modules"; "${env:ProgramFiles(x86)}\WindowsPowerShell\Modules"; "$env:ProgramFiles\WindowsPowerShell\Modules") -join ';'
+    Write-Output -InputObject "Adding Modules Path: $myPSmodPath to `$env:PSMODULEPATH"
+    $env:PSMODULEPATH += ';' + $myPSmodPath
+    $env:PSMODULEPATH
 }
 
-# try to update PS help files, if we have local admin role/rights 
+# try to update PS help files, if we have local admin role/rights
+Write-Output -InputObject 'Checking if PS Help files are due to be updated' 
 if (([security.principal.windowsprincipal] [security.principal.windowsidentity]::GetCurrent()).isinrole([Security.Principal.WindowsBuiltInRole] 'Administrator'))
 {
-    Get-Module -ListAvailable | Where HelpInfoUri | sort -Property Name -Unique | foreach {"Attempting to update-help -Module $($PSItem.Name)"; update-help -Module $($PSItem.Name)}
+    if (-not ($Global:PSState))
+    {
+        Write-Warning -Message "Fatal Error loading PowerShell saved state info from custom object: $PSState"
+    }
+    # Check $PSHelpUpdatedDate, from previously saved state; should be loaded from json file into variable by Sperry     
+    $PSHelpUpdatedDate = Get-Date -Date ($Global:PSState.HelpUpdatedDate -as [DateTime])
+    Write-Output -InputObject "PS Help (Last) Updated Date: $PSHelpUpdatedDate"
+    $NextUpdateDate = $PSHelpUpdatedDate.AddDays(10)
+    Write-Debug -Message "PS Help Next Update Date: $NextUpdateDate"
+    # Is today on or after $NextUpdateDate ?
+    if ($NextUpdateDate -ge (Get-Date)) {
+        # We DON'T need to Update Help right now
+        $updateNow = $false
+        Write-Debug -Message "We DON'T need to Update Help right now"
+    }
+    else
+    {
+        Write-Debug -Message "Iterate through current modules that have a HelpInfoUri defined, and attempt to update their help"
+        # Iterate through current modules that have a HelpInfoUri defined, and attempt to update their help 
+        Get-Module -ListAvailable | Where HelpInfoUri | sort -Property Name -Unique | foreach {"Update-Help -Module $($PSItem.Name)"; Update-Help -Module $($PSItem.Name)}
+
+        Write-Debug -Message "Update `$PSHelpUpdatedDate to today"
+        $PSHelpUpdatedDate = (Get-Date -DisplayHint Date -Format d)
+        # Update custom object property, and write to settings state file 
+        Write-Debug -Message "`$PSHelpUpdatedDate is $PSHelpUpdatedDate"
+        $Global:PSState.HelpUpdatedDate = $PSHelpUpdatedDate
+        Write-Debug -Message "`$Global:PSState is $Global:PSState"
+
+        Set-Content -Path $PSProgramsDir\Microsoft.PowerShell_state.json -Value ($Global:PSState | ConvertTo-Json) -Confirm
+    }
 }
 else
 {
-    Write-Output -InputObject "Skipping update-help, because we're either not on Windows, or do not have admin permissions`nConsider using get-help [term] -Online"
+    Write-Log -Message "Skipping update-help, because we're either not on Windows, or do not have admin permissions" # `nConsider using get-help [term] -Online"
 }
 
+Write-Output -InputObject ''
 # dot-source script file containing Get-NetSite function
 . .\Scripts\NetSiteName.ps1
 # Write-Output -InputObject 'Network connection info:'
 $NetInfo = Get-NetSite
 Write-Output -InputObject "Connected at Site: $($NetInfo.SiteName) (Address: $($NetInfo.IPAddress))" # | Select-Object -First 1))"
 
-<# -suspending until PS v4
+Write-Output -InputObject 'Importing function Out-Copy'
 # dot-source Out-Copy function script
 . .\Scripts\out-copy.ps1
 
-# dot-source Out-Highligt function script
+Write-Output -InputObject 'Importing function Out-Highlight'
+# dot-source Out-Highlight function script
 . .\Out-Highlight.ps1
-#>
 
+Write-Output -InputObject ''
 # Prompt to backup log files
 Write-Output -InputObject 'Archive PowerShell logs'
 Backup-Logs
 
+Write-Output -InputObject ''
+Write-Output -InputObject 'Updating this window title'
 Set-WindowTitle
 
+Write-Output -InputObject 'Declaring function Find-UpdatedDSCResource'
 function Find-UpdatedDSCResource {
 
 $MyDSCmodules = Get-Module -ListAvailable | Where-Object {'DSC' -in $_.Tags} | Select-Object -Property Name,Version
@@ -176,6 +216,7 @@ $MyDSCmodules = Get-Module -ListAvailable | Where-Object {'DSC' -in $_.Tags} | S
 
 # Find-UpdatedDSCResources
 
+Write-Output -InputObject 'Declaring function Get-PSGalleryModule'
 function Get-PSGalleryModule
 {
     Find-Module -Repository psgallery | Sort-Object -Descending -Property PublishedDate | Select-Object -First 30 | Format-List Name, PublishedDate, Description, Version | Out-Host -Paging
@@ -185,15 +226,78 @@ function Get-PSGalleryModule
 # re-open Visual Studio code PowerShell extension examples
 # & code $env:USERPROFILE\.vscode\extensions\ms-vscode.PowerShell\examples
 
+Write-Output -InputObject 'Declaring function Open-Code'
+function Open-Code
+{
+<#
+    Potential enhancements, as examples of code.exe / code-insiders.exe parameters
+        --install-extension guosong.vscode-util --install-extension ms-vscode.PowerShell --install-extension Shan.code-settings-sync --install-extension wmaurer.change-case --install-extension DavidAnson.vscode-markdownlint
+        --install-extension LaurentTreguier.vscode-simple-icons --install-extension seanmcbreen.Spell --install-extension mohsen1.prettify-json --install-extension ms-vscode.Theme-MarkdownKit 
+
+    Visual Studio Code - Insiders 1.8.0-insider
+
+    Usage: code-insiders.exe [options] [paths...]
+
+    Options:
+    -d, --diff                  Open a diff editor. Requires to pass two file
+                                paths as arguments.
+    -g, --goto                  Open the file at path at the line and column (add
+                                :line[:column] to path).
+    --locale <locale>           The locale to use (e.g. en-US or zh-TW).
+    -n, --new-window            Force a new instance of Code.
+    -p, --performance           Start with the 'Developer: Startup Performance'
+                                command enabled.
+    -r, --reuse-window          Force opening a file or folder in the last active
+                                window.
+    --user-data-dir <dir>       Specifies the directory that user data is kept
+                                in, useful when running as root.
+    --verbose                   Print verbose output (implies --wait).
+    -w, --wait                  Wait for the window to be closed before
+                                returning.
+    --extensions-dir <dir>      Set the root path for extensions.
+    --list-extensions           List the installed extensions.
+    --show-versions             Show versions of installed extensions, when using
+                                --list-extension.
+    --install-extension <ext>   Installs an extension.
+    --uninstall-extension <ext> Uninstalls an extension.
+    --disable-extensions        Disable all installed extensions.
+    --disable-gpu               Disable GPU hardware acceleration.
+    -v, --version               Print version.
+    -h, --help                  Print usage.
+
+#>
+    param (
+        [Parameter(Position=0)]
+        [array]
+        $ArgumentList
+    )
+
+    Write-Debug -Message 'Start Code Insiders'
+    if ($ArgumentList)
+    {
+        # sanitize passed parameters
+        $ArgsArray = @()
+        foreach ($filePath in @($ArgumentList -split ','))
+        {
+            if (test-path -Path $filePath -PathType Leaf)
+            {
+                $ArgsArray += $filePath
+            }
+        }
+    }
+    Write-Debug -Message "& code-insiders.cmd $ArgsArray"
+    & 'C:\Program Files (x86)\Microsoft VS Code Insiders\bin\code-insiders.cmd' $ArgsArray
+}
+
+Write-Output -InputObject 'Declaring aliases and XenApp shortcut functions'
 # Setup PS aliases for launching common apps, including XenApp
+New-Alias -Name psedit -Value Open-Code -ErrorAction Ignore
+
 New-Alias -Name rdp -Value Start-RemoteDesktop -ErrorAction Ignore
 
 # New-Alias -Name pa_start -Value $env:SystemDrive\SWTOOLS\Start.exe -Description 'start PortableApps' -Option Constant
-
 # New-Alias -Name pa_firefox -Value $env:SystemDrive\SWTOOLS\PortableApps\FirefoxPortable\FirefoxPortable.exe -Description 'Start PortableApp : Firefox' -Option Constant
-
 # New-Alias -Name pa_chrome -Value $env:SystemDrive\SWTOOLS\PortableApps\GoogleChromePortable\GoogleChromePortable.exe -Description 'Start PortableApp : Chrome browser' -Option Constant
-
 # New-Alias -Name pa_edit -Value C:\SWTOOLS\PortableApps\SublimeText3Portable\App\SublimeText3Portable\sublime_text.exe -Description 'Start PortableApp : SublimeText editor' -Option Constant
 
 New-Alias -Name rename -Value Rename-Item
@@ -203,6 +307,7 @@ function xa_assyst
     write-output -inputobject 'Start-XenApp -Qlaunch assyst'
     Start-XenApp -Qlaunch assyst
 }
+
 function xa_cmd
 {
     write-output -inputobject 'Start-XenApp -Qlaunch cmd'
